f166e7a50adda1a6af586c9f22699b4342cec854
huguesJEAN18 <h.jean@xefi.fr>
2024-10-09 10:21:06 +0200

    add legal page

---
+++ LoginPage/loginPage.js
@@ -0,0 +1,21 @@
+const twilio = require('twilio');
+
+const numeroPhone= document.getElementById('test');
+const accountSid = 'TON_ACCOUNT_SID';
+const authToken = 'TON_AUTH_TOKEN';
+const client = twilio(accountSid, authToken);
+
+// Fonction pour envoyer un SMS
+function sendSms(to, message) {
+  client.messages
+    .create({
+      body: message,
+      from:numeroPhone,  // Ton numéro Twilio
+      to: to,  // Numéro du destinataire
+    })
+    .then(message => console.log(`Message envoyé avec SID: ${message.sid}`))
+    .catch(error => console.error('Erreur lors de l\'envoi du message:', error));
+}
+
+// Exemple d'envoi
+sendSms(numeroPhone, 'Voici une notification depuis Twilio');
